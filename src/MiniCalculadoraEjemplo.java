e44154ac4dd2c92df74cc9d6732dcc6cbd5e23fe
jesuscanterodam10 <jesuscanterodam@gmail.com>
2026-04-06 12:37:09 +0200

    Metodo Valor Absoluto

--- src/MiniCalculadoraEjemplo.java
+++ src/MiniCalculadoraEjemplo.java
@@ -4,4 +4,8 @@ public class MiniCalculadoraEjemplo {
     public double raizCuadrada(double numero) {
         return Math.sqrt(numero);
     }
+
+    public double valorAbsoluto(double numero) {
+        return Math.abs(numero);
+    }
 }
