cafd036f5bdb58218a2bd83d5152fe629d104638
jesuscanterodam10 <jesuscanterodam@gmail.com>
2026-04-09 10:27:00 +0200

    Commit Documentacion Metodo Raiz Cuadrada

--- src/MiniCalculadoraEjemplo.java
+++ src/MiniCalculadoraEjemplo.java
@@ -10,6 +10,14 @@
 
 public class MiniCalculadoraEjemplo {
 
+    /**
+     * Calcula la raíz cuadrada de un número.
+     * * @param numero El valor para calcular la raíz
+     * @return El resultado de la operación
+     * @throws ArithmeticException Si ocurre un error de cálculo
+     * @see Math#sqrt(double)
+     */
+
     public double raizCuadrada(double numero) {
         return Math.sqrt(numero);
     }
