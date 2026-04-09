0b5c1e8b3074e615a2e389856721a52ee6950818
jesuscanterodam10 <jesuscanterodam@gmail.com>
2026-04-09 10:25:23 +0200

    Commit Documentacion Clase MiniCalculadora

--- src/MiniCalculadoraEjemplo.java
+++ src/MiniCalculadoraEjemplo.java
@@ -1,13 +1,21 @@
 
+/**
+ * Esta clase proporciona funciones matemáticas simples para el ejemplo
+ * * @author Jesus Cantero
+ * @version 1.0
+ * @since 1.0 (Indicativo numérico)
+ * @see Math
+ * @serial campo_descripcion
+ */
+
 public class MiniCalculadoraEjemplo {
 
     public double raizCuadrada(double numero) {
         return Math.sqrt(numero);
     }
 
+
     public double valorAbsoluto(double numero) {
         return Math.abs(numero);
     }
-
-
 }
